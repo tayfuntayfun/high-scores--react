377c7bc4f65e4f71ec1429b01a5ad699785fac13
Tayfun Aydogan <40833117+tayfuntayfun@users.noreply.github.com>
2020-06-01 14:30:05 +0100

    Updaet to Level-4

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 const App = () => {
   const [scoreOrder, setScoreOrder] = useState("*Click for ascending order*");
+  const [country, setCountry] = useState(allCountryScores);
   
   
   const handleButtonClick = () => {
@@ -16,12 +17,19 @@ const App = () => {
       setScoreOrder ("*Click for ascending order*")
     }
   }
+
+  const handleInputChange = (e) => {
+    const findCountry = e.target.value.toLowerCase();
+    setCountry (allCountryScores.filter(item => item.name.toLowerCase().includes(findCountry)))
+  }
+  
   return (
     <div className="App">
       <h1>High Scores per Country</h1>
-        <button name="button" className="button" onClick={handleButtonClick}> {scoreOrder}
+      <button name="button" className="button" onClick={handleButtonClick}> {scoreOrder}
       </button>
-        {allCountryScores
+      <input type="text" className="input" onChange={handleInputChange} placeholder='find country' style={{margin: '10px 0', height: '25px' }}/>
+        {country
           .sort((a, b) => a.name > b.name ? 1 : -1)
           .map(element => <Scoreboard
             name= {element.name}
